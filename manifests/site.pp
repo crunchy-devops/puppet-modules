0e50a80ac6c03dfbb0a30b7a5fb40d7f54a82ea4
Herve <dockerlite@gmail.com>
2023-03-23 16:42:31 +0100

    adding exec solution

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,7 @@
 #  content  => "Hello world\n",
 #}
 $my_directory = '/etc/puppetlabs/puppet/'
-$directory_contents = generate("/usr/bin/ls ${my_directory}")
+$directory_contents = generate('/usr/bin/ls /etc/puppetlabs/puppet/')
 
 $directory_contents.each |$item| {
   notify{ $item :
