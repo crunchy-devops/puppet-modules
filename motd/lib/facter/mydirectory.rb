52ddae8facdf3ee4260f38033dafc7c0fa215669
Herve <dockerlite@gmail.com>
2023-03-24 06:31:24 +0100

    adding exec solution

--- motd/lib/facter/mydirectory.rb
+++ motd/lib/facter/mydirectory.rb
@@ -1,6 +1,3 @@
 Facter.add('mydirectory') do
-  setcode do
-    # Replace "/path/to/my/directory" with the path to your directory
-    Dir.entries('/etc/puppetlabs/puppet/').reject { |f| File.directory? f }
-  end
+  setcode 'ls /etc/puppetlabs/puppet'
 end
