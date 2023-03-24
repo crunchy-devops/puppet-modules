f412d780b99d0774fe4210edf82797e30f97df05
Herve <dockerlite@gmail.com>
2023-03-24 10:09:14 +0100

    adding exec solution

--- motd/lib/facter/mydirectory.rb
+++ motd/lib/facter/mydirectory.rb
@@ -1,3 +1,7 @@
-Facter.add('mydirectory') do
-  setcode 'ls /etc/puppetlabs/puppet'
+# hardware_platform.rb
+
+Facter.add('hardware_platform') do
+  setcode do
+    Facter::Core::Execution.execute('/bin/uname --hardware-platform')
+  end
 end
